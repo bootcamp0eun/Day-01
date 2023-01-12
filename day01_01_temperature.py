e41271780a59696eabc1412189131fd51931bd26
unknown <bootcamp0eun@gmail.com>
2023-01-12 17:14:50 +0900

    01 temperature

---
+++ day01_01_temperature.py
@@ -0,0 +1,5 @@
+# (100°F − 32) × 5/9 = 37.778°C
+fahrenheit = float(input('화씨 온도 : '))
+# 변수 이름은 조금 길더라도 의미를 담도록
+celsius = (fahrenheit - 32.0) * (5.0/9.0)
+print(f'화씨 온도 {fahrenheit}도는 섭씨 온도 {celsius}입니다')

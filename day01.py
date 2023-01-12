bd10daac63902d6fed2fefb5c408394f6fdce77a
unknown <bootcamp0eun@gmail.com>
2023-01-12 17:43:48 +0900

    04 gui

--- day01.py
+++ day01.py
@@ -1,17 +1,6 @@
-# 교재 딕셔너리 예제
-subjects = {
-    '의사소통영어' : 'A+',
-    '오래된 미래' : 'B+',
-    '양자역학' : 'A0',
-    }
-student = '김도훈'
-subject = '오래된 미래'
-print(student, '학생의', subject, '과목 성적은', subjects[subject], '입니다.')  # 길어지면 복잡함
-# ultra modern style
-# f 스트링 (가장 최신 문법)
-print(f'{student} 학생의 {subject} 과목 성적은 {subjects[subject]}입니다.')
-# old style
-print("%s 학생의 %s 과목 성적은 %s입니다." % (student, subject, subjects[subject]))
-# modern style (format 함수)
-print("{0} 학생의 {1} 과목 성적은 {2}입니다.". format(student, subject, subjects[subject]))
-print("{} 학생의 {} 과목 성적은 {}입니다.". format(student, subject, subjects[subject]))
+import tkinter as tk
+
+win = tk.Tk()
+win.geometry('400x300')
+win.title('파이썬 1일차 Preview')
+win.mainloop()
